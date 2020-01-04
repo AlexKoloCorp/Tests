9b3242f066a9edf279ad4c5d968f7581d98d388d
AlexKoloCorp <kolomiets.alex.home@gmail.com>
2020-01-04 21:26:19 +0200

    some func

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -3,11 +3,14 @@
 
 using namespace std;
 int factorial(int);
-
+int getSum(int);
 
 int main()
 {
-	cout << factorial(3) << endl;
+	int input;
+	cin >> input;
+	getSum(input);
+	//cout << factorial(3) << endl;
 	system("pause");
 }
 int factorial(int N) {
@@ -18,5 +21,14 @@ int factorial(int N) {
 		return 0;
 	}
 	return N* factorial(N-1);
+}
+int getSum(int a) {
+	int sum = 0;
+	for (int i = 0; i < a + 1; i++) {
+		cout << sum << "+" << i << "=";
+		sum += i;
+	}
+	cout << "(" << sum << ")" << endl;
+	return sum;
 }
 
