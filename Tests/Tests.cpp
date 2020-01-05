c28a3004e8a17313862c8b91938317614ae09ab0
AlexKoloCorp <kolomiets.alex.home@gmail.com>
2020-01-05 19:42:12 +0200

    swap foo

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -5,17 +5,12 @@ using namespace std;
 int factorial(int);
 int getSum(int);
 int myFunc(int, int);
+void pointerFoo(int*, int*);
 
-int myFunc(int from, int to) {
-	if (from == to)
-	{
-		return from;
-	}
-	if (from > to)
-	{
-		return 0;
-	}
-	return from+myFunc(from+1,to);
+void pointerFoo(int *pa, int *pb) {
+	int tmp=*pa;
+	*pa = *pb;
+	*pb = tmp;
 }
 int main()
 {	
@@ -23,6 +18,10 @@ int main()
 	int var = 3[arr + 1] - arr[5] + (arr - 1)[4];
 	int *pvar = &var;
 	cout << *(pvar)+1 << endl;
+	int a=3, b=5;
+	cout << "a=" << a << "\t b=" << b << endl;
+	pointerFoo(&a, &b);
+	cout << "a=" << a << "\t b=" << b << endl;
 	//int input;
 	//cin >> input;
 	//cout << myFunc(0,input) << endl;
@@ -39,6 +38,19 @@ int factorial(int N) {
 	}
 	return N* factorial(N-1);
 }
+
+int myFunc(int from, int to) {
+	if (from == to)
+	{
+		return from;
+	}
+	if (from > to)
+	{
+		return 0;
+	}
+	return from + myFunc(from + 1, to);
+}
+
 int getSum(int a) {
 	int sum = 0;
 	for (int i = 0; i < a + 1; i++) {
